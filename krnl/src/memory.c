f5b05f4d948eaf5a47c016839ad5147ec5df160b
XKITZ7 <xkitz75@gmail.com>
2025-10-03 15:59:24 +0300

    Update memory.c

--- krnl/src/memory.c
+++ krnl/src/memory.c
@@ -1,1 +1,18 @@
+#include "memory.h"
 
+void* memset(void* ptr, int value, size_t num) {
+    uint8_t* p = (uint8_t*)ptr;
+    for (size_t i = 0; i < num; i++) {
+        p[i] = (uint8_t)value;
+    }
+    return ptr;
+}
+
+void* memcpy(void* destination, const void* source, size_t num) {
+    uint8_t* dst = (uint8_t*)destination;
+    const uint8_t* src = (const uint8_t*)source;
+    for (size_t i = 0; i < num; i++) {
+        dst[i] = src[i];
+    }
+    return destination;
+}
