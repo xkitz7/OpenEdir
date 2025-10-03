f929bb2a46a11004f71600bb24098d81220fe232
XKITZ7 <xkitz75@gmail.com>
2025-10-03 15:44:40 +0300

    Update multiboot.asm

--- boot/multiboot.asm
+++ boot/multiboot.asm
@@ -2,37 +2,29 @@
 section .multiboot
 align 4
 
-; Multiboot header constants
 MULTIBOOT_HEADER_MAGIC equ 0x1BADB002
-MULTIBOOT_HEADER_FLAGS equ 0x00000003  ; Align modules and provide memory map
+MULTIBOOT_HEADER_FLAGS equ 0x00000003
 MULTIBOOT_HEADER_CHECKSUM equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
 
-; Multiboot header
 multiboot_header:
     dd MULTIBOOT_HEADER_MAGIC
     dd MULTIBOOT_HEADER_FLAGS
     dd MULTIBOOT_HEADER_CHECKSUM
-
-    ; Additional fields (optional)
-    dd multiboot_header          ; header_addr
-    dd _start                    ; load_addr
-    dd 0                         ; load_end_addr
-    dd 0                         ; bss_end_addr
-    dd _start                    ; entry_addr
+    dd multiboot_header
+    dd _start
+    dd 0
+    dd 0
+    dd _start
 
 section .bss
 align 16
 
-; Stack configuration
 stack_bottom:
-    resb 16384                   ; 16 KB stack
+    resb 16384
 stack_top:
 
-; Multiboot information structure pointer
 multiboot_info_ptr:
     resd 1
-
-; Multiboot magic value
 multiboot_magic:
     resd 1
 
@@ -40,39 +32,28 @@ section .text
 global _start
 
 _start:
-    ; Save multiboot information
-    mov [multiboot_magic], eax   ; Multiboot magic number
-    mov [multiboot_info_ptr], ebx ; Multiboot info structure
-
-    ; Set up stack
+    mov [multiboot_magic], eax
+    mov [multiboot_info_ptr], ebx
     mov esp, stack_top
-
-    ; Clear direction flag
     cld
 
-    ; Initialize essential subsystems
+    ; Initialize systems
     call init_gdt
     call init_idt
-    call init_pic
-
-    ; Call kernel main function
+    
+    ; Call kernel main
     extern kernel_main
-    push ebx                     ; Multiboot info structure
-    push eax                     ; Multiboot magic number
+    push ebx
+    push eax
     call kernel_main
 
-    ; If kernel_main returns, halt
     cli
 .hang:
     hlt
     jmp .hang
 
-; Initialize Global Descriptor Table
 init_gdt:
-    ; Load GDT
     lgdt [gdt_ptr]
-    
-    ; Reload segment registers
     jmp 0x08:.reload_cs
 .reload_cs:
     mov ax, 0x10
@@ -83,59 +64,22 @@ init_gdt:
     mov ss, ax
     ret
 
-; Initialize Interrupt Descriptor Table
 init_idt:
-    ; Load IDT
     lidt [idt_ptr]
     ret
 
-; Initialize Programmable Interrupt Controller
-init_pic:
-    ; Remap PIC
-    mov al, 0x11
-    out 0x20, al                 ; Initialize master PIC
-    out 0xA0, al                 ; Initialize slave PIC
-    
-    mov al, 0x20
-    out 0x21, al                 ; Master PIC vector offset
-    mov al, 0x28
-    out 0xA1, al                 ; Slave PIC vector offset
-    
-    mov al, 0x04
-    out 0x21, al                 ; Tell master PIC about slave
-    mov al, 0x02
-    out 0xA1, al                 ; Tell slave PIC cascade identity
-    
-    mov al, 0x01
-    out 0x21, al                 ; Set master PIC to 8086 mode
-    out 0xA1, al                 ; Set slave PIC to 8086 mode
-    
-    ; Mask all interrupts initially
-    mov al, 0xFF
-    out 0x21, al
-    out 0xA1, al
-    
-    ret
-
-; GDT
 section .data
 align 4
 
 gdt:
-    ; Null descriptor
     dq 0x0000000000000000
-    
-    ; Code segment descriptor
     dq 0x00CF9A000000FFFF
-    
-    ; Data segment descriptor
     dq 0x00CF92000000FFFF
 
 gdt_ptr:
     dw $ - gdt - 1
     dd gdt
 
-; IDT (initialized to zero for now)
 idt:
     times 256 dq 0
 
