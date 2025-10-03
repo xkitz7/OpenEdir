54ee8e0852910a941777b441b7c6c39f5b7e7d7c
XKITZ7 <xkitz75@gmail.com>
2025-10-03 15:46:46 +0300

    Update memory.h

--- krnl/include/memory.h
+++ krnl/include/memory.h
@@ -1,1 +1,9 @@
+#ifndef MEMORY_H
+#define MEMORY_H
 
+#include "types.h"
+
+void* memset(void* ptr, int value, size_t num);
+void* memcpy(void* destination, const void* source, size_t num);
+
+#endif
